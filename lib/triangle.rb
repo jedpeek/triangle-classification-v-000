e7e87aad368239f3335bdcd29ec35a0fea6c864d
Jed Peek <jedpeek@yahoo.com>
2018-06-14 23:39:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ attr_accessor :side1, :side2, :side3
   def kind
     if @side1 == 0 || @side2 == 0 || @side3 == 0
       #Raise Error
+    end
     if @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
