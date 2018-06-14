d6bd31bf8bf51474debf39709dd5671adb77cde1
Jed Peek <jedpeek@yahoo.com>
2018-06-14 23:49:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ attr_accessor :side1, :side2, :side3
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       #Raise Error Negative Sides
     elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
-      #Raise Error Triangle Inequality
+      raise TriangleError
     end
     if @side1 == @side2 && @side2 == @side3
       :equilateral
