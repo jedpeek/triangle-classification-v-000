e3111e46606e55dffc82e7b601c239527d3d0590
Jed Peek <jedpeek@yahoo.com>
2018-06-14 23:44:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,5 +18,8 @@ attr_accessor :side1, :side2, :side3
 
   class TriangleError < StandardError
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+
+    elsif @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+
   end
 end
